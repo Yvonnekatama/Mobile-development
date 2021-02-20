8f813c7d7e6ef8fc333c64412b52a2f66fe246c9
Yvonnekatama <katamayvonne@gmail.com>
2021-02-21 00:30:36 +0300

    Assignment 1

---
+++ src/Assignment 1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var studentName="Yvonne Katama"
+    var studentAge="22"
+    var studentContacts="0718748585"
+    var studentWeightInKg="50.567439"
+    var studentNationality= "true"
+    println(studentName)
+    println(studentAge)
+    println(studentContacts)
+    println(studentWeightInKg)
+    println(studentNationality)
+    var student=arrayOf("Yvonne Katama",22,"0718748585",50.567439,"true")
+}
